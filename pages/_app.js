f6941c836d28071e88d9bf6c5a1da86f9e9cd199
dai-gi <daisuke.u524@gmail.com>
2021-05-05 22:38:00 +0900

    font awesome 追加

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }) {
     <Layout>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous"></link>
       </Head>
       <Component {...pageProps} />
     </Layout>
