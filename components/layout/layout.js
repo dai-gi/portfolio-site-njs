272acdd93e154696a11a7a23e409cf854038481e
dai-gi <daisuke.u524@gmail.com>
2021-05-06 05:55:29 +0900

    while editing

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,9 +1,27 @@
 import Navigation from "./navigation";
+import Link from "next/link";
 
 export default function Layout(props) {
     return (
         <div className="flex flex-col min-h-screen">
             <Navigation />
+            <section className="mx-auto container flex items-center justify-end w-full h-7">
+                <Link href="/">
+                    <a className="fab fa-twitter text-xl text-gray-500 mr-7"></a>
+                </Link>
+                <Link href="/">
+                    <a className="fab fa-facebook-f text-xl text-gray-500"></a>
+                </Link>
+            </section>
+            <section className="h-screen items-center grid grid-cols-2 bg-gray-200">
+                <div className="">
+                    <h1 className="text-5xl mr-40">DAISUKE UEDA</h1>
+                    <p>駆け出しエンジニア</p>
+                </div>
+                <div className="text-9xl flex justify-center">
+                    <i className="fas fa-user-circle"></i>
+                </div>
+            </section>
             <main className="mx-auto px-5 max-w-screen-lg">
                 {props.children}
             </main>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head';
-import Link from "next/link";
-
 
 export default function Home() {
   return (
@@ -8,23 +6,6 @@ export default function Home() {
       <Head>
         <title>UD Portfolio</title>
       </Head>
-      <div className="flex items-center justify-end w-full h-7">
-        <Link href="/">
-          <a className="fab fa-twitter text-xl text-gray-500 mr-7"></a>
-        </Link>
-        <Link href="/">
-          <a className="fab fa-facebook-f text-xl text-gray-500"></a>
-        </Link>
-      </div>
-      <div className="h-screen items-center grid grid-cols-2 bg-gray-200">
-        <div className="">
-          <h1 className="text-5xl mr-40">DAISUKE UEDA</h1>
-          <p>駆け出しエンジニア</p>
-        </div>
-        <div className="text-9xl flex justify-center">
-          <i className="fas fa-user-circle"></i>
-        </div>
-      </div>
     </div>
   );
 }
