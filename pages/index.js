8452318245eb92a5b61e59951bf06aafb420dce1
dai-gi <daisuke.u524@gmail.com>
2021-05-07 05:57:09 +0900

    headingまで

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,11 @@ export default function Home() {
       <Head>
         <title>UD Portfolio</title>
       </Head>
+      <section>
+        <div className="h-28 flex items-center mt-24 mb-10">
+          <h1 className="text-5xl font-thin">Works</h1>
+        </div>
+      </section>
     </div>
   );
 }
