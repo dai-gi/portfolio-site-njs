016a9fd78d3de5a3f345eae8d53964de262a3572
dai-gi <daisuke.u524@gmail.com>
2021-05-10 09:01:13 +0900

    footerを下に固定

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -5,7 +5,7 @@ export default function Layout(props) {
     return (
         <div className="flex flex-col min-h-screen">
             <Navigation />
-            <section className="mx-auto px-32 container flex items-center justify-end w-full h-7">
+            <section className="mx-auto px-32 container flex items-center justify-end w-full h-7 mb-3">
                 <Link href="/">
                     <a className="fab fa-twitter text-xl text-gray-500 mr-7"></a>
                 </Link>
@@ -22,12 +22,24 @@ export default function Layout(props) {
                     <i className="fas fa-user-circle text-gray-400"></i>
                 </div>
             </section>
-            <main className="h-screen mx-auto px-5 max-w-screen-lg">
-                {props.children}
-            </main>
-            <footer className="container flex items-center justify-end mx-auto w-full h-36 text-sm font-light px-32">
-                ©︎ UD All rights reserved.
-            </footer>
+            <body className="flex flex-col min-h-screen">
+                <main className="mx-auto px-5 max-w-screen-lg flex-grow">
+                    {props.children}
+                </main>
+                <footer className="container grid grid-cols-12 mx-auto w-full h-36 text-sm font-light">
+                    <nav className="col-span-8 flex items-center">
+                        <Link href="/">
+                            <a className="mr-20 font-light">Home</a>
+                        </Link>
+                        <Link href="/about">
+                            <a className="font-light">About</a>
+                        </Link>
+                    </nav>
+                    <div className="col-span-4 flex items-center justify-end">
+                        ©︎ UD All rights reserved.
+                    </div>
+                </footer>
+            </body>
         </div>
     );
 }
