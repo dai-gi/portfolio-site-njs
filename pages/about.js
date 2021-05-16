7b9c8238c29faad8e8905de5f085f3d35787fc87
dai-gi <daisuke.u524@gmail.com>
2021-05-17 06:11:49 +0900

    レイアウト編集中

--- pages/about.js
+++ pages/about.js
@@ -6,11 +6,11 @@ export default function About({ profiles }) {
             <div className="h-28 flex items-center justify-center mt-10">
                 <h1 className="text-5xl font-thin">About</h1>
             </div>
-            <section className="grid grid-cols-2 h-64">
-                <div className="flex items-center justify-end mr-12">
+            <section className="grid grid-cols-12 h-64">
+                <div className="flex items-center justify-end mr-2 col-span-5">
                     <i className="fas fa-user text-8xl text-gray-300 bg-gray-200 h-28 w-28 flex items-end justify-center"></i>
                 </div>
-                <div className="flex items-center">
+                <div className="flex items-center col-span-7 ml-5">
                     <div>
                         <p className="mb-1">{profiles.map((profile) => profile.name)}</p>
                         <p className="mb-1">Twitter:<a className="ml-9" href={profiles.map((profile) => profile.twitter)}>dai_twii</a></p>
@@ -18,9 +18,12 @@ export default function About({ profiles }) {
                         <p>学習言語:<span className="ml-5">{profiles.map((profile) => profile.programming)}</span></p>
                     </div>
                 </div>
-                <div className="flex items-center justify-center bg-gray-400 w-screen">
-                    <div className="w-5/12">
-                        <p>{profiles.map((profile) => profile.introduction)}</p>
+                <div className="flex items-center justify-center w-screen h-60">
+                    <div className="w-4/12">
+                        <p>約9年間、建設会社で勤めていました。</p>
+                        <br></br>
+                        <p>2020年1月プログラミング学習をスタート</p>
+                        <p>WEB開発エンジニアを目指し、日々、WEB制作やアプリケーション開発をしています。</p>
                     </div>
                 </div>
             </section>
