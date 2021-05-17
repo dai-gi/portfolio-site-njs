80846e82edce01145e6ccda6f2b3cb9dc8d7ca7f
dai-gi <daisuke.u524@gmail.com>
2021-05-18 05:20:07 +0900

    aboutページ完成

--- pages/about.js
+++ pages/about.js
@@ -3,10 +3,10 @@ import { getAllProfilesData } from "../lib/profiles"
 export default function About({ profiles }) {
     return (
         <div className="flex-grow">
-            <div className="h-28 flex items-center justify-center mt-10">
+            <div className="h-28 flex items-center justify-center m-10">
                 <h1 className="text-5xl font-thin">About</h1>
             </div>
-            <section className="grid grid-cols-12 h-64">
+            <div className="grid grid-cols-12 h-32">
                 <div className="flex items-center justify-end mr-2 col-span-5">
                     <i className="fas fa-user text-8xl text-gray-300 bg-gray-200 h-28 w-28 flex items-end justify-center"></i>
                 </div>
@@ -18,15 +18,17 @@ export default function About({ profiles }) {
                         <p>学習言語:<span className="ml-5">{profiles.map((profile) => profile.programming)}</span></p>
                     </div>
                 </div>
-                <div className="flex items-center justify-center w-screen h-60">
-                    <div className="w-4/12">
-                        <p>約9年間、建設会社で勤めていました。</p>
-                        <br></br>
-                        <p>2020年1月プログラミング学習をスタート</p>
-                        <p>WEB開発エンジニアを目指し、日々、WEB制作やアプリケーション開発をしています。</p>
-                    </div>
+            </div>
+            <div className="flex items-center justify-center w-screen h-60">
+                <div className="w-5/12">
+                    <p>約9年間、建設会社で勤めていました。</p><br></br>
+                    <p>2020年1月プログラミング学習をスタート</p><br></br>
+                    <p>WEB開発エンジニアを目指し、日々、WEB制作やアプリケーション開発をしています。</p>
                 </div>
-            </section>
+            </div>
+            <div className="flex items-center justify-center mt-20">
+                <a className="far fa-arrow-alt-circle-up text-gray-300 text-3xl" href="/about"></a>
+            </div>
         </div>
     );
 }
