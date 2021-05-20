a3f14cee0d6e8e3dd68d29efd18bbba7061a8129
dai-gi <daisuke.u524@gmail.com>
2021-05-20 23:14:42 +0900

    production data一覧を表示まで完了

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import Head from 'next/head';
+
 import { getAllProfilesData } from "../lib/profiles"
 
-export default function Home({ profiles }) {
+import Production from "../components/production/production"
+import { getAllProductionsData } from "../lib/productions"
+
+
+export default function Home({ profiles, productions }) {
   return (
     <div className="flex-grow">
       <Head>
         <title>UD Portfolio</title>
       </Head>
-      <section className="grid grid-cols-2 bg-gray-100 h-64">
+      <div className="grid grid-cols-2 bg-gray-100 h-80">
         <div className="flex items-center justify-end">
           <div>
             <h1 className="text-4xl">{profiles.map((profile) => profile.title)}</h1>
@@ -17,37 +22,26 @@ export default function Home({ profiles }) {
         <div className="flex items-center ml-28">
           <i className="fas fa-user-circle text-8xl text-gray-300"></i>
         </div>
-      </section>
-      <main className="mx-auto px-5 max-w-screen-lg">
+      </div>
+      <main className="mx-auto px-5 max-w-screen-lg mb-10">
         <div className="h-28 flex items-center justify-center mt-24 mb-10">
           <h1 className="text-5xl font-thin">Works</h1>
         </div>
         <div className="grid grid-cols-12">
-          <div class="max-w-sm rounded border overflow-hidden drop-shadow-md col-span-4 m-4">
-            {/* <img class="w-full" src="../img/a1.JPG" alt="Sunset in the mountains"> */}
-            <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-              <p class="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-              </p>
-            </div>
-            <div class="px-6 pt-4 pb-2">
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-            </div>
-          </div>
+          {productions && productions.map((production) => <Production key={production.id} production={production} />)}
         </div>
       </main>
     </div>
   );
 }
 
+
 export async function getStaticProps() {
   const profiles = await getAllProfilesData();
+  const productions = await getAllProductionsData();
 
   return {
-    props: {profiles},
+    props: { profiles, productions },
     revalidate: 3,
   };
 }
