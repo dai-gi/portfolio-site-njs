fb73ae5170dfc370808e9f435c704ca5db46fb29
dai-gi <daisuke.u524@gmail.com>
2021-05-21 06:40:23 +0900

    worksに変更

--- components/production/production.js
+++ components/production/production.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Production({production}) {
     return (
-        <Link href={`/productions/${production.id}`}>
+        <Link href={`/works/${production.id}`}>
             <a className="max-w-sm overflow-hidden drop-shadow-md col-span-4 m-4">
                 <img className="w-full border rounded" src={production.image} alt="Portfolio1" />
                 <div className="px-6 py-4 flex justify-center">
