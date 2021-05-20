382bfc502de6b279c806d4f806b913053748075c
dai-gi <daisuke.u524@gmail.com>
2021-05-20 23:13:51 +0900

    initial commit

---
+++ components/production/production.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Production({production}) {
+    return (
+        <Link href={`/productions/${production.id}`}>
+            <a className="max-w-sm overflow-hidden drop-shadow-md col-span-4 m-4">
+                <img className="w-full border rounded" src={production.image} alt="Portfolio1" />
+                <div className="px-6 py-4 flex justify-center">
+                    <p className="text-xl mb-2">{production.title}</p>
+                </div>
+            </a>
+        </Link>
+    );
+}
