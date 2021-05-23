e3ced44bc9245c94331dc230edac44fd720fa955
dai-gi <daisuke.u524@gmail.com>
2021-05-23 23:24:14 +0900

    高さを変更

--- components/production/productions.js
+++ components/production/productions.js
@@ -4,7 +4,7 @@ export default function Production({production}) {
     return (
         <Link href={`/productions/${production.id}`}>
             <a className="max-w-sm overflow-hidden drop-shadow-md col-span-4 m-4">
-                <img className="w-full border rounded" src={production.image} alt="Portfolio1" />
+                <img className="w-full border rounded h-64" src={production.thumbnail} alt="Portfolio1" />
                 <div className="px-6 py-4 flex justify-center">
                     <p className="text-xl mb-2">{production.title}</p>
                 </div>
