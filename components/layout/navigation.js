f8cd6f9f52796b2dba7c64b4262f54928defb0b1
dai-gi <daisuke.u524@gmail.com>
2021-05-26 21:01:50 +0900

    ロゴのデザインを変更

--- components/layout/navigation.js
+++ components/layout/navigation.js
@@ -4,10 +4,10 @@ export default function Navigation() {
     return (
         <header className="container flex flex-row items-center mx-auto py-10 max-w-screen-lg text-gray-800">
             <Link href="/">
-                <div className="border-black border h-12 w-9 relative">
+                <div className="border-black border-2 h-12 w-12 relative">
                     <a className="text-4xl logo">
-                        <span>D</span>
-                        <span className="absolute left-2 top-2">U.</span>
+                        <span className="absolute left-1">D</span>
+                        <span className="absolute left-3 top-2">U<span className="ml-3">.</span></span>
                     </a>
                 </div>
             </Link>
