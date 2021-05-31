dd56736a77fbaa98896442486b7253b6c9f5a1bc
dai-gi <daisuke.u524@gmail.com>
2021-06-01 05:52:22 +0900

    ナビバーのレスポンシブデザイン完成

--- components/layout/navigation.js
+++ components/layout/navigation.js
@@ -2,40 +2,59 @@ import Link from "next/link";
 
 export default function Navigation() {
     return (
-        <header className="container flex flex-row items-center sm:mx-auto py-10 max-w-screen-lg px-8 text-gray-800 md:px-10">
-            <Link href="/">
-                <div className="border-black border-2 h-12 w-12 relative">
-                    <a className="text-4xl logo">
-                        <span className="absolute left-1">D</span>
-                        <span className="absolute left-3 top-2">U<span className="ml-3">.</span></span>
-                    </a>
+        <header className="container mx-auto py-10 px-8 max-w-screen-lg text-gray-800">
+            <div className="flex items-center justify-between">
+                <Link href="/">
+                    <div className="border-black border-2 h-12 w-12 relative">
+                        <a className="text-4xl logo">
+                            <span className="absolute left-1">D</span>
+                            <span className="absolute left-3 top-2">U<span className="ml-3">.</span></span>
+                        </a>
+                    </div>
+                </Link>
+                <div className="sm:hidden">
+                    <div>
+                        <button type="button" className="inline-flex items-center justify-end p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500" aria-controls="mobile-menu" aria-expanded="false">
+                            <span className="sr-only">Open main menu</span>
+
+                            <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+
+                            <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+
+                        </button>
+                    </div>
                 </div>
-            </Link>
-            <div class="sm:hidden ml-auto">
-                <button type="button" class="inline-flex items-center justify-end p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500" aria-controls="mobile-menu" aria-expanded="false">
-                    {/* <span class="sr-only">Open main menu</span> */}
-                    <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
-                    <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
+                <nav className="hidden sm:ml-auto sm:block">
+                    <Link href="/">
+                        <a className="mr-10 font-light">Home</a>
+                    </Link>
+                    <Link href="#about">
+                        <a className="mr-10 font-light">About</a>
+                    </Link>
+                    <Link href="#work">
+                        <a className="mr-10 font-light">Work</a>
+                    </Link>
+                    <Link href="/">
+                        <a className="font-light">Contact</a>
+                    </Link>
+                </nav>
             </div>
-            <nav className="hidden sm:ml-auto sm:block">
-                <Link href="/">
-                    <a className="mr-10 font-light">Home</a>
-                </Link>
-                <Link href="#about">
-                    <a className="mr-10 font-light">About</a>
-                </Link>
-                <Link href="#work">
-                    <a className="mr-10 font-light">Work</a>
-                </Link>
-                <Link href="/">
-                    <a className="font-light">Contact</a>
-                </Link>
-            </nav>
+            <div className="sm:hidden mt-10" id="mobile-menu">
+                <div className="px-2 pt-2 pb-3 space-y-1">
+
+                    <a href="#" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
+
+                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
+
+                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+
+                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+                </div>
+            </div>
         </header>
         // <header>
         //     <div class="mx-auto px-2 sm:px-6 lg:px-8">
