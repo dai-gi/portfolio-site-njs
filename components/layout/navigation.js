8856a5f7bae524b902f0efa0de687ab2889583ac
dai-gi <daisuke.u524@gmail.com>
2021-06-09 06:05:56 +0900

    レスポンシブ修正

--- components/layout/navigation.js
+++ components/layout/navigation.js
@@ -19,24 +19,37 @@ export default function Navigation() {
                         </div>
                     </a>
                 </Link>
-                <div className="sm:hidden">
+                <nav className="hidden md:block md:ml-auto">
+                    <Link href="/">
+                        <a className="mr-10 font-light">Home</a>
+                    </Link>
+                    <Link href="#about">
+                        <a className="mr-10 font-light">About</a>
+                    </Link>
+                    <Link href="#work">
+                        <a className="mr-10 font-light">Work</a>
+                    </Link>
+                    <Link href="/">
+                        <a className="font-light">Contact</a>
+                    </Link>
+                </nav>
+                <div className="md:hidden">
                     <div>
                         <button type="button" className="inline-flex items-center justify-end p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500" aria-controls="mobile-menu" aria-expanded="false" onClick={handleClick}>
                             <span className="sr-only">Open main menu</span>
-
-                            <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            {/* Humberger icon */}
+                            <svg className={`${active ? 'hidden' : 'block'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
-
-                            <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            {/* X icon */}
+                            <svg className={`${active ? 'block' : 'hidden'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                             </svg>
-
                         </button>
                     </div>
                 </div>
             </div>
-            <div className={`${active ? '' : 'hidden'} mt-10`} id="mobile-menu">
+            <div className={`${active ? '' : 'hidden'} mt-10`}>
                 <div className="px-2 pt-2 pb-3 space-y-1">
                     <Link href="/">
                         <a href="#" className="text-gray-700 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Home</a>
