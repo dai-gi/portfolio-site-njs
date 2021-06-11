0cbb13e722f2a5dccd4a6bd3c54083cf54e64648
dai-gi <daisuke.u524@gmail.com>
2021-06-12 06:10:49 +0900

    footerの折り畳みを作成中

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,7 +1,15 @@
 import Navigation from "./navigation";
 import Link from "next/link";
 
+import React, {useState} from "react";
+
 export default function Layout(props) {
+
+    const [active, setActive] = useState(false);
+    const handleClick = () => {
+        setActive(!active);
+    };
+
     return (
         <div className="flex flex-col min-h-screen">
             <Navigation />
@@ -9,6 +17,7 @@ export default function Layout(props) {
                 {props.children}
                 <footer className="text-sm font-light text-gray-800">
                     <div className="grid grid-cols-12 mx-auto max-w-screen-lg px-5 h-36 md:px-10">
+                        <button></button>
                         <nav className="col-span-12 md:col-span-8 flex items-center">
                             <Link href="/">
                                 <a className="mr-10 font-light">Home</a>
