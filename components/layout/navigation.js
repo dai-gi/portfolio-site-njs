3e91226260b49dbd844b37db1ce4febbb0168d48
dai-gi <daisuke.u524@gmail.com>
2021-06-13 22:48:30 +0900

    不要箇所を削除

--- components/layout/navigation.js
+++ components/layout/navigation.js
@@ -35,8 +35,7 @@ export default function Navigation() {
                 </nav>
                 <div className="md:hidden">
                     <div>
-                        <button type="button" className="inline-flex items-center justify-end p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500" aria-controls="mobile-menu" aria-expanded="false" onClick={handleClick}>
-                            <span className="sr-only">Open main menu</span>
+                        <button type="button" className="inline-flex items-center justify-end p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500" onClick={handleClick}>
                             {/* Humberger icon */}
                             <svg className={`${active ? 'hidden' : 'block'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
