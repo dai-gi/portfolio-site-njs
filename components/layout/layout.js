73b7abf2a521e583cac5f79887892a7a5097feee
dai-gi <daisuke.u524@gmail.com>
2021-06-14 06:19:15 +0900

    フッター編集中_1

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -17,8 +17,11 @@ export default function Layout(props) {
                 {props.children}
                 <div className="flex items-center justify-center text-gray-400 hover:text-gray-500 md:hidden">
                     <button type="button" className="h-5 w-5 focus:outline-none" onClick={handleClick}>
-                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" class="svg-inline--fa fa-chevron-down fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <svg className={`${active ? 'hidden' : 'block'}`} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" class="svg-inline--fa fa-chevron-down fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                             <path className="fill-current text-gray-400 hover:text-gray-500" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
+                        </svg>
+                        <svg className={`${active ? 'block' : 'hidden'}`} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="minus" class="svg-inline--fa fa-minus fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            <path className="fill-current text-gray-400 hover:text-gray-500"  fill="currentColor" d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
                         </svg>
                     </button>
                 </div>
