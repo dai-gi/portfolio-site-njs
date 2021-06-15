4931974f3cae0dd64906d5bc8c4114ed7322e976
dai-gi <daisuke.u524@gmail.com>
2021-06-15 21:58:49 +0900

    ハンバーガーメニューにborderを追加

--- components/layout/navigation.js
+++ components/layout/navigation.js
@@ -35,7 +35,7 @@ export default function Navigation() {
                 </nav>
                 <div className="md:hidden">
                     <div>
-                        <button type="button" className="inline-flex items-center justify-end p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500" onClick={handleClick}>
+                        <button type="button" className="border border-gray-400 hover:border-gray-500 inline-flex items-center justify-end p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500" onClick={handleClick}>
                             {/* Humberger icon */}
                             <svg className={`${active ? 'hidden' : 'block'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
