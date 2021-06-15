570a33f06be55a42249b16f891289740fb1bfcc7
dai-gi <daisuke.u524@gmail.com>
2021-06-15 21:42:06 +0900

    borderの色を変更

--- components/production/productions.js
+++ components/production/productions.js
@@ -5,7 +5,7 @@ export default function Production({production}) {
         <Link href={`/productions/${production.id}`}>
             <a className="max-w-sm overflow-hidden drop-shadow-md text-gray-800 mt-8 col-span-12 md:mt-0 md:col-span-4">
                 <div className="flex justify-center">
-                    <img className="w-56 border border-gray-300 rounded h-56" src={production.thumbnail} alt="Portfolio1" />
+                    <img className="w-56 border border-gray-400 rounded h-56" src={production.thumbnail} alt="Portfolio1" />
                 </div>
                 <div className="px-6 py-4 flex justify-center">
                     <p className="text-xl mb-2">{production.title}</p>
