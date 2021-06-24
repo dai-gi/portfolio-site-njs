cc309865f0bfed61ec8703bfb833ad2f9df2579f
dai-gi <daisuke.u524@gmail.com>
2021-06-24 22:58:40 +0900

    クラスを整理

--- components/production/productions.js
+++ components/production/productions.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function Production({production}) {
     return (
         <Link href={`/productions/${production.id}`}>
-            <a className="max-w-sm overflow-hidden drop-shadow-md text-gray-800 mt-8 col-span-12 md:mt-0 md:col-span-4">
+            <a className="col-span-12 max-w-sm mt-8 text-gray-800 overflow-hidden drop-shadow-md md:col-span-4 md:mt-0">
                 <div className="flex justify-center">
-                    <img className="w-56 border border-gray-400 rounded h-56" src={production.thumbnail} alt="Portfolio1" />
+                    <img className="w-56 h-56 border border-gray-400 rounded" src={production.thumbnail} alt="Portfolio1" />
                 </div>
-                <div className="px-6 py-4 flex justify-center">
-                    <p className="text-xl mb-2">{production.title}</p>
+                <div className="flex justify-center my-4 text-xl">
+                    <p>{production.title}</p>
                 </div>
             </a>
         </Link>
