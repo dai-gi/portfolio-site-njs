d9abf2424262ed5ec26a7a7c6b6fb53b2f780cc9
dai-gi <daisuke.u524@gmail.com>
2021-06-28 21:15:31 +0900

    配色、ボックスサイズ、マージンを変更

--- components/layout/sns.js
+++ components/layout/sns.js
@@ -2,11 +2,11 @@ import { getAllProfilesData } from "../../lib/profiles";
 
 export default function Sns() {
     return (
-        <div className="container mx-auto my-7 max-w-screen-lg text-gray-800">
-            <div className="flex justify-end my-10">
+        <div className="container max-w-screen-lg mx-auto my-4 text-gray-500">
+            <div className="flex justify-end text-2xl">
                 <div>
-                    <a className="fab fa-twitter mr-10 md:text-2xl" href="https://twitter.com/dai_twii"></a>
-                    <a className="fab fa-github md:text-2xl" href="https://github.com/dai-gi"></a>
+                    <a className="fab fa-twitter mr-10" href="https://twitter.com/dai_twii"></a>
+                    <a className="fab fa-github" href="https://github.com/dai-gi"></a>
                 </div>
             </div>
         </div>
