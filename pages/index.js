e3b5498862a781ff4593bf922ed2c5931095c9e3
dai-gi <daisuke.u524@gmail.com>
2021-06-29 22:26:11 +0900

    クラスの順番を変更、不要な開業を削除

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head';
-
 import { getAllProfilesData } from "../lib/profiles";
-
 import Production from "../components/production/productions";
 import { getAllProductionsData } from "../lib/productions";
 
@@ -14,13 +12,13 @@ export default function Home({ profiles, productions }) {
       <div className="flex items-center justify-center mt-64 mb-96">
         <h1 className="text-6xl md:text-9xl text-gray-600 title">{profiles.map((profile) => profile.title)}</h1>
       </div>
-      <div className="mx-auto max-w-screen-lg my-10 md:my-20 grid grid-cols-12 bg-gray-100 py-20">
+      <div className="grid grid-cols-12 max-w-screen-lg my-10 mx-auto py-20 bg-gray-100 md:my-20">
         <div className="flex items-center justify-center col-span-12">
-          <h1 className="text-4xl font-thin text-gray-800 border-b border-gray-700 w-16 flex justify-center pb-3 mb-16 md:text-5xl" id="about">About</h1>
+          <h1 className="flex justify-center w-16 pb-3 mb-16 text-4xl font-thin text-gray-800 border-b border-gray-700 md:text-5xl" id="about">About</h1>
         </div>
-        <div className="col-span-12 md:col-span-6 flex justify-center md:justify-end items-center">
+        <div className=" flex justify-center items-center col-span-12 md:col-span-6 md:justify-end">
           <div className="border border-gray-300 md:mr-16">
-            <img className="object-cover h-40 w-40 md:h-44 md:w-44" src={profiles.map((profile) => profile.subimage)}/>
+            <img className="h-40 w-40 object-cover md:h-44 md:w-44" src={profiles.map((profile) => profile.subimage)}/>
           </div>
         </div>
         <div className="col-span-12 mt-10 flex justify-center text-gray-700 md:justify-start md:col-span-6 md:mt-6 md:ml-10">
@@ -39,9 +37,9 @@ export default function Home({ profiles, productions }) {
           </div>
         </div>
       </div>
-      <main className="mx-auto px-5 max-w-screen-lg mt-20 grid grid-cols-12">
+      <main className="grid grid-cols-12 px-5 mt-20 mx-auto max-w-screen-lg">
         <div className="flex items-center justify-center col-span-12">
-          <h1 className="text-4xl font-thin text-gray-800 border-b border-gray-700 w-16 flex justify-center pb-3 mb-16 md:text-5xl" id="work">Work</h1>
+          <h1 className="flex justify-center w-16 pb-3 mb-16 text-4xl text-gray-800 font-thin border-b border-gray-700 md:text-5xl" id="work">Work</h1>
         </div>
         {productions && productions.map((production) => <Production key={production.id} production={production} />)}
       </main>
