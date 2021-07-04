b87b5ee594f1f1f331f242d6227cd1bba6b24a33
dai-gi <daisuke.u524@gmail.com>
2021-07-04 13:11:31 +0900

    アイコンの間隔を変更

--- components/layout/sns.js
+++ components/layout/sns.js
@@ -3,8 +3,7 @@ export default function Sns() {
         <div className="container max-w-screen-lg mx-auto my-4 text-gray-500">
             <div className="flex justify-end text-2xl">
                 <div>
-                    {/* <a className="fab fa-twitter mr-10" href="https://twitter.com/dai_twii"></a> */}
-                    {/* <a className="fab fa-twitter mr-10" href={profiles.map((profile) => profile.twitter)}></a> */}
+                    <a className="fab fa-twitter mr-5" href="https://twitter.com/dai_twii"></a>
                     <a className="fab fa-github" href="https://github.com/dai-gi"></a>
                 </div>
             </div>
