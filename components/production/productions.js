b51c27550543f873e7bfba9eed955c3bb697cd0c
dai-gi <daisuke.u524@gmail.com>
2021-07-05 14:41:02 +0900

    roundedクラスを削除

--- components/production/productions.js
+++ components/production/productions.js
@@ -5,7 +5,7 @@ export default function Production({production}) {
         <Link href={`/productions/${production.id}`}>
             <a className="col-span-12 max-w-sm mt-8 text-gray-800 overflow-hidden drop-shadow-md md:col-span-4 md:mt-0">
                 <div className="flex justify-center">
-                    <img className="w-56 h-56 border border-gray-400 rounded" src={production.thumbnail} alt="Portfolio" />
+                    <img className="w-56 h-56 border border-gray-400" src={production.thumbnail} alt="Portfolio" />
                 </div>
                 <div className="flex justify-center my-4 text-xl">
                     <p>{production.title}</p>
