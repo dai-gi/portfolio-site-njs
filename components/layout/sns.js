a457bcf3cdf57a25a65cd467f07bcdc0bc67a8b1
dai-gi <daisuke.u524@gmail.com>
2021-07-05 17:52:58 +0900

    ホバー時のテキストカラーを設定

--- components/layout/sns.js
+++ components/layout/sns.js
@@ -3,8 +3,8 @@ export default function Sns() {
         <div className="container max-w-screen-lg mx-auto my-4 text-gray-500">
             <div className="flex justify-end text-2xl">
                 <div>
-                    <a className="fab fa-twitter mr-5" href="https://twitter.com/dai_twii"></a>
-                    <a className="fab fa-github" href="https://github.com/dai-gi"></a>
+                    <a className="fab fa-twitter mr-5 hover:text-gray-400" href="https://twitter.com/dai_twii"></a>
+                    <a className="fab fa-github hover:text-gray-400" href="https://github.com/dai-gi"></a>
                 </div>
             </div>
         </div>
