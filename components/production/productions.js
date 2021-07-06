79a00e32ea715940181130c4d1af87695e36da8a
dai-gi <daisuke.u524@gmail.com>
2021-07-06 23:12:39 +0900

    hover時にopacity-70を設定

--- components/production/productions.js
+++ components/production/productions.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Production({production}) {
     return (
         <Link href={`/productions/${production.id}`}>
-            <a className="col-span-12 max-w-sm mt-8 text-gray-800 overflow-hidden drop-shadow-md md:col-span-4 md:mt-0">
+            <a className="col-span-12 max-w-sm mt-8 text-gray-800 overflow-hidden drop-shadow-md md:col-span-4 md:mt-0 hover:opacity-70">
                 <div className="flex justify-center">
                     <img className="w-56 h-56 border border-gray-400" src={production.thumbnail} alt="Portfolio" />
                 </div>
