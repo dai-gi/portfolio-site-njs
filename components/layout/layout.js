0adf3f32ea4353cf5bc97cca74d124fa17d4a3a7
dai-gi <daisuke.u524@gmail.com>
2021-07-06 23:16:43 +0900

    hover:opacity-70を付与

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -13,7 +13,7 @@ export default function Layout( props ) {
                 </div>
                 <div className="flex items-center justify-center">
                     <Link href="/" >
-                        <a href="#" className="flex items-center justify-center h-20 w-20 my-10 text-gray-500 hover:text-gray-400 md:my-20">
+                        <a href="#" className="flex items-center justify-center h-20 w-20 my-10 text-gray-500 hover:opacity-70 md:my-20">
                             <div className="w-4 h-4">
                                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-double-up" class="svg-inline--fa fa-angle-double-up fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                                     <path className="" fill="currentColor" d="M177 255.7l136 136c9.4 9.4 9.4 24.6 0 33.9l-22.6 22.6c-9.4 9.4-24.6 9.4-33.9 0L160 351.9l-96.4 96.4c-9.4 9.4-24.6 9.4-33.9 0L7 425.7c-9.4-9.4-9.4-24.6 0-33.9l136-136c9.4-9.5 24.6-9.5 34-.1zm-34-192L7 199.7c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l96.4-96.4 96.4 96.4c9.4 9.4 24.6 9.4 33.9 0l22.6-22.6c9.4-9.4 9.4-24.6 0-33.9l-136-136c-9.2-9.4-24.4-9.4-33.8 0z"></path>
