0205e8e7c79e9bf8560052f7860cd6a87e070e32
dai-gi <daisuke.u524@gmail.com>
2021-07-11 13:55:09 +0900

    トップページのレスポンシブデザイン完成

--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,21 @@ export default function Home({ profiles, productions }) {
         <h1 className="text-6xl md:text-8xl xl:text-9xl text-gray-600 title">{profiles.map((profile) => profile.title)}</h1>
       </div>
       <main>
+        <hr className="mb-20" />
         <div className="flex items-center justify-center">
           <h1 className="flex justify-center w-16 pb-3 text-4xl text-gray-800 font-thin border-b border-gray-700 md:text-5xl" id="work">Work</h1>
         </div>
         <div className="grid grid-cols-12 px-5 mt-9 mx-auto max-w-screen-lg md:mt-20">
           {productions && productions.map((production) => <Production key={production.id} production={production} />)}
         </div>
-        <div className="flex justify-center mt-44 mb-20">
-          <div className="float-left w-28 h-28 md:w-32 md:h-32">
-            <img className="w-28 h-28 md:w-32 md:h-32 object-cover border border-gray-300" src={profiles.map((profile) => profile.subimage)}/>
-          </div>
-          <div className="flex items-center ml-6 md:ml-10 text-gray-700">
-            <p className="text-sm md:text-base">{profiles.map((profile) => profile.name)}</p>
+        <div className="flex justify-center">
+          <div className="grid justify-items-center w-screen md:w-7/12 md:flex md:justify-center md:px-20 py-10 mt-32 mb-16 border bg-gray-100">
+            <div className="flex justify-center w-24 h-24 md:w-32 md:h-32">
+              <img className="w-24 h-24 md:w-32 md:h-32 object-cover border border-gray-300" src={profiles.map((profile) => profile.subimage)}/>
+            </div>
+            <div className="flex items-center justify-center mt-5 md:ml-10 text-gray-700">
+              <p className="text-sm md:text-base">{profiles.map((profile) => profile.name)}</p>
+            </div>
           </div>
         </div>
       </main>
